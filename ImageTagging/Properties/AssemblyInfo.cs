ad95d27716319f2b308de4e880337f341b54ebf8
pavo76 <dpavicic93@gmail.com>
2017-05-18 12:58:13 +0200

    Initial Commit
    Initial Commit

---
+++ ImageTagging/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ImageTagging")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ImageTagging")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84573db3-3711-46e5-b6de-b988f34c3420")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ImageTagging/Scripts/ImageClick.js
@@ -0,0 +1,129 @@
+﻿// variable for storing pressed coordinates
+var coordsString = "";
+// variable for naming id of new area element
+var areaIDIndex = 0;
+
+// adds coordinates of clicked position in image to the coordsString variable
+// adds temporary area if temporary area with id="tempArea" doesn't exist
+// else it adds new coordinates to temporary area
+function imageOnClick(elementRef) {
+    var posObject = getEventLocation(event);
+    if (coordsString == "") {
+        coordsString = posObject.x + ", " + posObject.y;
+    }
+    else if (coordsString != "") {
+        coordsString = coordsString + ", " + posObject.x + ", " + posObject.y
+    }
+    if (!CheckIfAreaExists("tempArea")) {
+        AddTempArea("map")
+    }
+    else if (CheckIfAreaExists("tempArea")) {
+        EditTempArea();
+    }
+}
+
+// returns coordinates of position in image where mouse was clicked
+function getEventLocation(eventRef) {
+    var positionX = 0;
+    var positionY = 0;
+    var $img = $(eventRef.target);
+    var offset = $img.offset();
+    if (eventRef.pageX) {
+        positionX = eventRef.pageX - offset.left;
+        positionY = eventRef.pageY - offset.top;
+    }
+    else if (window.event) {
+        positionX = eventRef.clientX + document.body.scrollLeft;
+        positionY = eventRef.clientY + document.body.scrollTop;
+    }
+    else if (eventRef.clientX) {
+        positionX = eventRef.clientX;
+        positionY = eventRef.clientY;
+    }
+
+    return { x: positionX, y: positionY };
+}
+
+// Clears variable coordsString
+function ClearCoordsString() {
+    coordsString = "";
+    RemoveArea("tempArea", "map");
+    Hilight();
+}
+
+// adds Area child element to map with id="mapID"
+// id of Area is set to area + areaIDIndex 
+// areaIDIndex autoincrements for each area added
+// removes temporary area with id="tempArea" from map
+// clears coordsString
+function AddArea(mapID) {
+    var map = document.getElementById(mapID);
+
+    RemoveArea("tempArea", mapID);
+    area = document.createElement("area");
+    area.shape = "poly";
+    area.coords = coordsString;
+    area.href = "#";
+    area.title = "da";
+    area.alt = "da";
+    var areaID = "area" + areaIDIndex;
+    area.setAttribute("id", areaID);
+    map.appendChild(area);
+
+    areaIDIndex += 1;
+    ClearCoordsString();
+    Hilight();
+}
+
+
+//  removes area with id="areaID" from map with id="mapID""
+function RemoveArea(areaID, mapID) {
+    var map = document.getElementById(mapID);
+    map.removeChild(document.getElementById(areaID))
+}
+
+// checks if an area element with id="areaID" exists
+// returns true if it exists, otherwise returns false
+function CheckIfAreaExists(areaID) {
+    var exists = !!document.getElementById(areaID);
+    if (exists) {
+        return true;
+    }
+    else
+        return false;
+}
+
+// adds temporary area with id="tempArea" on map with id="mapID"
+function AddTempArea(mapID) {
+    var map = document.getElementById(mapID);
+
+    area = document.createElement("area");
+    area.shape = "poly";
+    area.coords = coordsString;
+    area.href = "#";
+    area.title = "da";
+    area.alt = "da";
+    area.setAttribute("id", "tempArea");
+    map.appendChild(area);
+    Hilight();
+}
+
+
+// changes coordinates of temporary area to current coordsString
+function EditTempArea() {
+    var area = document.getElementById("tempArea");
+    area.coords = coordsString;
+    Hilight();
+}
+
+
+// Hilights te areas of image map using JQuery plugin maphilight
+function Hilight() {
+    $(function () {
+        $('.map').maphilight({
+            fillColor: '008800',
+            alwaysOn: true
+        });
+    });
+}
+
